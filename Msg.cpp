052d6780f7d9ca0da4440f4c41e1097d83d9a842
minhoJeongT <37164086+minhoJeongT@users.noreply.github.com>
2018-03-22 16:13:20 +0900

    3/22

--- Msg.cpp
+++ Msg.cpp
@@ -4,42 +4,78 @@
 #include <string.h>
 #include <iostream>
 #include "Msg.h"
-
+#include <queue>
 
 using namespace std;
 
-void Msg::unpacking(char packet[]){
+
+
+
+Msg::Msg(){
+ header=0;
+
+    
+};
+
+
+void Msg::enQueue(char packet[],int strlen){
+
+    for (int i=0; i<strlen; i++){
+        unfinished.push(packet[i]);
+    }
+}
+
+int Msg::seperateHeader(){
+
+    if (header==0){
+        if(unfinished.size()>sizeof(header)){  
+            //header가 0이고 queue에 header크기 이상의 char 가 있을때
         
-    memcpy(&header,packet,sizeof(header)); 
+            char temp[4];
+            for(int i=0; i<4; i++){
+                temp[i]=unfinished.front();
+                unfinished.pop();
+            }
+            memcpy(&header,temp,4);
+            return 1;
+        }else{
+            //header가 0인데 아직 header크기만큼 char를 못 받은 경우
+            return 0;
+        }
+    }else{
+        //header가 0이 아닐때
+        return -1;
+    }
+}
 
-    char* target = message;
-	char* source = packet+sizeof(header);
-	strcpy(target, source);
 
-   }
+int Msg::unpacking(){
+    
+    if(header<=unfinished.size()){
+        for (int i = 0; i<header; i++){
+        message[i]=unfinished.front();
+        unfinished.pop();
+        }
+        return 1;    
+    }else{
+        return 0;
+    }    
+}
     
  
 void Msg::packing(char buf[]){
 		
 	int header=0;
     for (int i=0; i<sizeof(buf);i++)
-    {
-        if(buf[i]==0){
-            break;
-        }
+    {   
+         if(buf[i]==0){
+             break;
+         }
         header++;
     }
 
 	char* target = message+sizeof(header);
 	char* source = buf;
 	memcpy(message,&header,sizeof(header));
-	strcpy(target,source);
-	
+	strcpy(target,source);	
 }
-
-
-
-
-
-
-
