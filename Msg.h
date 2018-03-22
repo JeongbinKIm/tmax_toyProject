8c5131e78137cb87d984ba5f8762d6776176e592
minhoJeongT <37164086+minhoJeongT@users.noreply.github.com>
2018-03-23 00:22:32 +0900

    final

--- Msg.h
+++ Msg.h
@@ -1,20 +1,27 @@
 #ifndef _MSG_H_
 #define _MSG_H_
 
+#include <queue>
+using namespace std;
 #define BUF_SIZE 100
+#define NAME_SIZE 20
+#define HEADER_SIZE 4
 
-class Msg{
+class Msg
+{
 
+private:
 public:
-    Msg();
-    ~Msg();
-    int header;
-    char message[BUF_SIZE+50];
-    queue<char> unfinished;
-    int unpacking();
-	void packing(char buf[]);
-    int seperateHeader();
-    void enQueue(char packet[],int strlen);
+  int header;
+  char message[BUF_SIZE + NAME_SIZE + HEADER_SIZE];
+  queue<char> unfinished;
+  Msg();
+  ~Msg();
+  int unpacking();
+  void packing(char buf[]);
+  int seperateHeader();
+  void enQueue(char packet[], int strlen);
+  void packet(char *packet);
 };
 
 #endif
