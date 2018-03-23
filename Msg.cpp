3cac9e297eee1ac7b6484952d07fdd38bf5c6993
JeongbinKIm <37166706+JeongbinKIm@users.noreply.github.com>
2018-03-23 13:09:24 +0900

    Update Msg.cpp

--- Msg.cpp
+++ Msg.cpp
@@ -27,32 +27,37 @@ void Msg::enQueue(char packet[], int strlen)
     {
         unfinished.push(packet[i]);
     }
+    
     memset(message, 0, sizeof(message));
 }
 
 int Msg::seperateHeader()
 {
-
     if (header == 0)
     {
         if (unfinished.size() > sizeof(header))
         {
             //header가 0이고 queue에 header크기 이상의 char 가 있을때
             char temp[4];
+            
             for (int i = 0; i < 4; i++)
             {
                 temp[i] = unfinished.front();
                 unfinished.pop();
             }
+            
             memcpy(&header, temp, 4);
+            
             return 1;
         }
+        
         else
         {
             //header가 0인데 아직 header크기만큼 char를 못 받은 경우
             return 0;
         }
     }
+    
     else
     {
         //header가 0이 아닐때
@@ -63,6 +68,7 @@ int Msg::seperateHeader()
 int Msg::unpacking()
 {
     memset(message, 0, sizeof(message));
+    
     if (header <= unfinished.size())
     {
         for (int i = 0; i < header; i++)
@@ -70,8 +76,10 @@ int Msg::unpacking()
             message[i] = unfinished.front();
             unfinished.pop();
         }
+        
         return 1;
     }
+    
     else
     {
         return 0;
@@ -82,6 +90,7 @@ void Msg::packing(char buf[])
 {
     char *target = message + sizeof(header);
     char *source = buf;
+    
     memcpy(message, &header, sizeof(header));
     strcpy(target, source);
 }
